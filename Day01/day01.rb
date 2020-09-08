a7503647e9b46ec00243d03f39853d0b3aa3c307
molinarim <michael122096@hotmail.com>
2020-09-08 17:14:35 -0400

    hello

---
+++ Day01/day01.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+######### PART 1
+# Calculate how much fuel is needed for each module.
+file = File.open('input.txt')
+modules = file.readlines.map(&:chomp).map(&:to_i)
+file.close
+fuel = 0
+
+def calc_fuel(mod)
+  mod / 3 - 2
+end
+
+modules.each { |mod| fuel += calc_fuel(mod) }
+
+puts fuel
+
+########## PART 2
+# Calculate how much fuel is needed for each module while taking into account
+# that fuel adds weight and would need more fuel to launch.
+fuel = 0
+
+# mod is the same in calc_fuel method, acc is the accumulator for the fuel of
+# the module and the weight of the fuel.  If the calculated fuel is 0 or
+# negative then just return acc.
+def calc_weight(mod, acc)
+  fuel = calc_fuel(mod)
+  fuel <= 0 ? acc : calc_weight(fuel, acc + fuel)
+end
+
+modules.each { |mod| fuel += calc_weight(mod, 0) }
+
+puts fuel
