2bf1384b4697457749a6e6b21d286f23527b2b76
molinarim <michael122096@hotmail.com>
2020-09-11 02:12:37 -0400

    day03.rb produce wrong output when doing map, t.rb is correct

--- Day03/day03.rb
+++ Day03/day03.rb
@@ -22,7 +22,8 @@ p1 = Point.new(0, 0)
 p2 = Point.new(0, 0)
 
 # Change point's location based on direction. Store each step in visited.
-def step(p, d, m, visited)
+def step(p, d, m, visited, h)
+  s = 1
   m.times do
     case d
     when 'U'
@@ -35,25 +36,53 @@ def step(p, d, m, visited)
       p.y -= 1
     end
 
+    x = p.x
+    y = p.y
     visited << [p.x, p.y]
+
+    h[[x, y]] = s unless h[[x, y]] # h.key?([p.x, p.y])
+    s += 1
   end
 end
 
 visited1 = Set.new
 visited2 = Set.new
+h1 = {}
+h2 = {}
+steps1 = 0
+steps2 = 0
 
 # Used regex to get direction and number of steps taken.
 for i in 0...len
   pat.match(dir1[i])
   d1 = $1
   mag1 = $2.to_i
-  step(p1, d1, mag1, visited1)
+  step(p1, d1, mag1, visited1, h1)
   pat.match(dir2[i])
   d2 = $1
   mag2 = $2.to_i
-  step(p2, d2, mag2, visited2)
+  step(p2, d2, mag2, visited2, h2)
 end
 
+############### Part 1
 # Keep only the steps where there is overlap and find the closest to the origin.
 intersection = visited1 & visited2
-puts intersection.map { |x, y| x.abs + y.abs }.min
+#puts intersection.map { |x, y| x.abs + y.abs }.min
+
+############### Part 2
+# puts h1
+#puts h1.keys & h2.keys
+h_keys_inter = h1.keys & h2.keys
+#puts h_keys_inter
+puts h_keys_inter.map { |x, y| h1[[x, y]] + h2[[x, y]] }
+
+#h_intersection = h1.keep_if { |k, _v| h2.key?(k) }
+#h2.keep_if { |k, _v| h1.key?(k) }
+#puts h1.size
+#puts h2.size
+#step_intersection = 
+#h_intersection.each_key { }.min
+
+# a = {}
+# h_intersection.each { |k, v| a[k] = h2[k] + v }
+# puts a.values
